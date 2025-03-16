bafe70df67c64fd08f52d6ab76902bea88fc34ad
marinasbrum <125208855+marinasbrum@users.noreply.github.com>
2025-03-16 20:16:24 -0300

    Update gauss.c

--- C/gauss.c
+++ C/gauss.c
@@ -1,149 +1,153 @@
-package main
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <time.h>
 
-import (
-	"fmt"
-	"math/rand"
-	"os"
-	"strconv"
-	"time"
-)
+#define MAXN 2000 
+int N;
 
-const MAXN = 2000
+volatile float A[MAXN][MAXN], B[MAXN], X[MAXN];
 
-var N int
-var A [MAXN][MAXN]float64
-var B [MAXN]float64
-var X [MAXN]float64
+void gauss(); 
 
-func timeSeed() uint32 {
-	return uint32(time.Now().UnixNano() % int64(^uint(0)>>1))
+unsigned int time_seed() {
+  struct timeval t;
+  struct timezone tzdummy;
+
+  gettimeofday(&t, &tzdummy);
+  return (unsigned int)(t.tv_usec);
 }
 
-func parameters() {
-	seed := 0
-	rand.Seed(int64(timeSeed()))
+void parameters(int argc, char **argv) {
+  int seed = 0; 
 
-	if len(os.Args) == 3 {
-		var err error
-		seed, err = strconv.Atoi(os.Args[2])
-		if err != nil {
-			fmt.Println("Invalid seed input.")
-			os.Exit(1)
-		}
-		rand.Seed(int64(seed))
-		fmt.Printf("Random seed = %d\n", seed)
-	}
-	if len(os.Args) >= 2 {
-		var err error
-		N, err = strconv.Atoi(os.Args[1])
-		if err != nil {
-			fmt.Println("Invalid matrix dimension input.")
-			os.Exit(1)
-		}
-		if N < 1 || N > MAXN {
-			fmt.Printf("N = %d is out of range.\n", N)
-			os.Exit(1)
-		}
-	} else {
-		fmt.Printf("Usage: %s <matrix_dimension> [random seed]\n", os.Args[0])
-		os.Exit(1)
-	}
-	fmt.Printf("\nMatrix dimension N = %d.\n", N)
+  srand(time_seed());  
+
+  if (argc == 3) {
+    seed = atoi(argv[2]);
+    srand(seed);
+    printf("Random seed = %i\n", seed);
+  }
+  if (argc >= 2) {
+    N = atoi(argv[1]);
+    if (N < 1 || N > MAXN) {
+      printf("N = %i is out of range.\n", N);
+      exit(0);
+    }
+  }
+  else {
+    printf("Usage: %s <matrix_dimension> [random seed]\n",
+           argv[0]);
+    exit(0);
+  }
+
+  printf("\nMatrix dimension N = %i.\n", N);
 }
 
-func initializeInputs() {
-	fmt.Println("\nInitializing...")
-	for col := 0; col < N; col++ {
-		for row := 0; row < N; row++ {
-			A[row][col] = rand.Float64()
-		}
-		B[col] = rand.Float64()
-		X[col] = 0.0
-	}
+void initialize_inputs() {
+  int row, col;
+
+  printf("\nInitializing...\n");
+  for (col = 0; col < N; col++) {
+    for (row = 0; row < N; row++) {
+      A[row][col] = (float)rand() / 32768.0;
+    }
+    B[col] = (float)rand() / 32768.0;
+    X[col] = 0.0;
+  }
+
 }
 
-func printInputs() {
-	if N < 10 {
-		fmt.Println("\nA =")
-		for row := 0; row < N; row++ {
-			fmt.Printf("\t")
-			for col := 0; col < N; col++ {
-				fmt.Printf("%5.2f", A[row][col])
-				if col < N-1 {
-					fmt.Print(", ")
-				} else {
-					fmt.Println(";")
-				}
-			}
-		}
-		fmt.Print("\nB = [")
-		for col := 0; col < N; col++ {
-			fmt.Printf("%5.2f", B[col])
-			if col < N-1 {
-				fmt.Print("; ")
-			} else {
-				fmt.Println("]")
-			}
-		}
-	}
+void print_inputs() {
+  int row, col;
+
+  if (N < 10) {
+    printf("\nA =\n\t");
+    for (row = 0; row < N; row++) {
+      for (col = 0; col < N; col++) {
+	printf("%5.2f%s", A[row][col], (col < N-1) ? ", " : ";\n\t");
+      }
+    }
+    printf("\nB = [");
+    for (col = 0; col < N; col++) {
+      printf("%5.2f%s", B[col], (col < N-1) ? "; " : "]\n");
+    }
+  }
 }
 
-func printX() {
-	if N < 100 {
-		fmt.Print("\nX = [")
-		for row := 0; row < N; row++ {
-			fmt.Printf("%5.2f", X[row])
-			if row < N-1 {
-				fmt.Print("; ")
-			} else {
-				fmt.Println("]")
-			}
-		}
-	}
+void print_X() {
+  int row;
+
+  if (N < 100) {
+    printf("\nX = [");
+    for (row = 0; row < N; row++) {
+      printf("%5.2f%s", X[row], (row < N-1) ? "; " : "]\n");
+    }
+  }
 }
 
-func gauss() {
-	var multiplier float64
-	fmt.Println("Computing Serially.")
-	for norm := 0; norm < N-1; norm++ {
-		for row := norm + 1; row < N; row++ {
-			multiplier = A[row][norm] / A[norm][norm]
-			for col := norm; col < N; col++ {
-				A[row][col] -= A[norm][col] * multiplier
-			}
-			B[row] -= B[norm] * multiplier
-		}
-	}
-	for row := N - 1; row >= 0; row-- {
-		X[row] = B[row]
-		for col := N - 1; col > row; col-- {
-			X[row] -= A[row][col] * X[col]
-		}
-		X[row] /= A[row][row]
-	}
+int main(int argc, char **argv) {
+  struct timeval etstart, etstop;  
+  struct timezone tzdummy;
+  struct timespec etstart2, etstop2; 
+  unsigned long long usecstart, usecstop;
+
+  parameters(argc, argv);
+
+  initialize_inputs();
+
+  print_inputs();
+
+  printf("\nStarting clock.\n");
+  gettimeofday(&etstart, &tzdummy);
+  etstart2 = clock(); 
+
+  gauss();
+
+  gettimeofday(&etstop, &tzdummy);
+  etstop2 = clock(); 
+
+  printf("Stopped clock.\n");
+
+  usecstart = (unsigned long long)etstart.tv_sec * 1000000 + etstart.tv_usec;
+  usecstop = (unsigned long long)etstop.tv_sec * 1000000 + etstop.tv_usec;
+
+  print_X();
+
+  printf("\nElapsed time = %g ms.\n",
+	 (float)(usecstop - usecstart)/(float)1000);
+  printf("(CPU times are accurate to the nearest %g ms)\n",
+	 1.0/(float)CLOCKS_PER_SEC * 1000.0);
+  printf("My total CPU time = %g ms.\n",
+       (float)(etstop2 - etstart2) / CLOCKS_PER_SEC * 1000);
+  printf("--------------------------------------------\n");
+  exit(0);
 }
 
-func main() {
-	start := time.Now()
+void gauss() {
+  int norm, row, col;  
+  float multiplier;
 
-	startCPU := time.Now()
+  printf("Computing Serially.\n");
 
-	parameters()
+  for (norm = 0; norm < N - 1; norm++) {
+    for (row = norm + 1; row < N; row++) {
+      multiplier = A[row][norm] / A[norm][norm];
+      for (col = norm; col < N; col++) {
+	A[row][col] -= A[norm][col] * multiplier;
+      }
+      B[row] -= B[norm] * multiplier;
+    }
+  }
 
-	initializeInputs()
-
-	printInputs()
-
-	fmt.Println("\nStarting clock.")
-	gauss()
-
-	printX()
-
-	elapsed := time.Since(start)
-	fmt.Printf("\nElapsed time = %g ms.\n", float64(elapsed.Milliseconds()))
-	fmt.Printf("(CPU times are accurate to the nearest %g ms)\n", float64(time.Second)/float64(time.Millisecond))
-
-	elapsedCPU := time.Since(startCPU)
-	totalCPU := elapsedCPU.Seconds() * 1000
-	fmt.Printf("My total CPU time = %g ms.\n", totalCPU)
+  for (row = N - 1; row >= 0; row--) {
+    X[row] = B[row];
+    for (col = N-1; col > row; col--) {
+      X[row] -= A[row][col] * X[col];
+    }
+    X[row] /= A[row][row];
+  }
 }
